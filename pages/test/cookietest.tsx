0d88d855df4d2b8638b26d43b6c37349551fb589
Xeei <109275765+Xeei@users.noreply.github.com>
2025-06-04 20:23:02 +0700

    update

--- pages/test/cookietest.tsx
+++ pages/test/cookietest.tsx
@@ -2,29 +2,29 @@ import React, { useEffect } from 'react'
 import Cookies from 'js-cookie';
 import _createtoken from '@/utils/createtoken';
 
-function cookietest() {
+function Cookietest() {
 
-    let data = {
-        name: 'earth',
-        userid: '1234',
-        login: 1
+  let data = {
+    name: 'earth',
+    userid: '1234',
+    login: 1
+  }
+
+  useEffect(() => {
+
+
+    let str = _createtoken(data)
+    console.log(str);
+    if (str) {
+      Cookies.set('login', str, { secure: true, expires: 1 })
     }
 
-    useEffect(() => {
-
-        
-        let str = _createtoken(data)
-        console.log(str);
-        if(str){
-            Cookies.set('login', str, {secure: true, expires: 1})
-        }
-        
-        console.log(Cookies.get('login'));
-    },[])
+    console.log(Cookies.get('login'));
+  }, [])
 
   return (
     <div>cookietest</div>
   )
 }
 
-export default cookietest
+export default Cookietest
